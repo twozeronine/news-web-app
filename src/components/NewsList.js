064289c50b94f067436b3b5dcc58ff17ed138130
twozeronine <amdusias209@gmail.com>
2021-03-15 15:28:42 +0900

    Categories에서 NavLink 사용하기

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -16,13 +16,6 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: "제목",
-  description: "내용",
-  url: 'https"//google.com',
-  urlToImage: "https://via.placeholder.com/160",
-};
-
 function NewsList({ category }) {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
