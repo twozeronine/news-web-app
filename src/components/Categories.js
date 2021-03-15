1d964e97f7dcc59bee766bdd644c303dd990eb39
twozeronine <amdusias209@gmail.com>
2021-03-15 14:57:44 +0900

    카테고리 컴포넌트 UI에 스타일 적용

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const categories = [
   {
@@ -55,16 +55,33 @@ const Category = styled.div`
     color: #495057;
   }
 
+  // active 상태에 따라 스타일 변화
+  ${(props) =>
+    props.active &&
+    css`
+      font-weight: 600;
+      border-bottom: 2px solid #22b8cf;
+      color: #22b8cf;
+      &:hover: {
+        color: #3bc9db;
+      }
+    `}
   & + & {
     margin-left: 1rem;
   }
 `;
 
-function Categories() {
+function Categories({ onSelect, category }) {
   return (
     <CategoriesBlock>
       {categories.map((c) => (
-        <Category key={c.name}>{c.text}</Category>
+        <Category
+          key={c.name}
+          active={category === c.name}
+          onClick={() => onSelect(c.name)}
+        >
+          {c.text}
+        </Category>
       ))}
     </CategoriesBlock>
   );
